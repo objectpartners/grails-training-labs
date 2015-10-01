a0d6d3561eca325e6c7bc95ee6d474f8a4b2aa4d
Nick Larson <nicklas.larson@gmail.com>
2015-10-01 00:26:12 -0500

    format code

--- groovyWorkshops/src/test/groovy/Workshop1Spec.groovy
+++ groovyWorkshops/src/test/groovy/Workshop1Spec.groovy
@@ -1,12 +1,5 @@
 // Groovy Workshop #1
 
-// class Foo {
-//   String name
-//   boolean isBar
-// }
-//
-// def myFoo = new Foo(name: 'test', isBar: false);
-// assert myFoo.isBar
 import spock.lang.*
 
 class Workshop1Spec extends Specification {
@@ -18,17 +11,17 @@ class Workshop1Spec extends Specification {
   *   sayHi('Nick') would return 'Hello Nick!'
   */
   def "1.1 - string concatenation"() {
-      given:
-        def sayHi = {name ->
-          // TODO: Say Hello!
-        }
-      expect:
-        result == sayHi(name)
+    given:
+      def sayHi = {name ->
+        // TODO: Say Hello!
+      }
+    expect:
+      result == sayHi(name)
 
-      where:
-          result | name
-          'Hello Class!' | 'Class'
-          'Hello Students!' | 'Students'
+    where:
+      result            | name
+      'Hello Class!'    | 'Class'
+      'Hello Students!' | 'Students'
   }
 
   /*
@@ -36,14 +29,14 @@ class Workshop1Spec extends Specification {
   *   the class are detailed in the comments of that file.
   */
   def "1.2 - create person class"() {
-      when:
-          def person = new Person()
+    when:
+      def person = new Person()
 
-      then:
-          person.hasProperty('firstName')
-          person.hasProperty('lastName')
-          person.hasProperty('middleName')
-          person.hasProperty('age')
+    then:
+      person.hasProperty('firstName')
+      person.hasProperty('lastName')
+      person.hasProperty('middleName')
+      person.hasProperty('age')
   }
 
   /*
@@ -53,18 +46,18 @@ class Workshop1Spec extends Specification {
   */
   @Unroll
   def "1.3 - person sayName() - #result"() {
-      given:
-          def person = new Person(firstName: firstName, lastName: lastName, middleName: middleName)
+    given:
+      def person = new Person(firstName: firstName, lastName: lastName, middleName: middleName)
 
-      expect:
-          person.sayName() == result
+    expect:
+      person.sayName() == result
 
-      where:
-          firstName | lastName | middleName | result
-          'Sterling' | 'Archer' | 'Malory' | 'Sterling Malory Archer'
-          'Lana' | 'Kane' | 'Anthony' | 'Lana Anthony Kane'
-          'Malory' | 'Archer' | 'Duchess' | 'Malory Duchess Archer'
-          'Cyril' | 'Figgis' | null | 'Cyril Figgis'
-          'Pam' | 'Poovey' | null | 'Pam Poovey'
+    where:
+      firstName   | lastName  | middleName  | result
+      'Sterling'  | 'Archer'  | 'Malory'    | 'Sterling Malory Archer'
+      'Lana'      | 'Kane'    | 'Anthony'   | 'Lana Anthony Kane'
+      'Malory'    | 'Archer'  | 'Duchess'   | 'Malory Duchess Archer'
+      'Cyril'     | 'Figgis'  | null        | 'Cyril Figgis'
+      'Pam'       | 'Poovey'  | null        | 'Pam Poovey'
   }
 }
