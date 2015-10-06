ad8ddeb5b24d6a89d5cf43a2a03644dde7accba1
Nick Larson <nicklas.larson@gmail.com>
2015-10-06 17:14:05 -0500

    went too far

--- grailsWorkshop/questionApp/src/test/groovy/com/opi/VoteControllerSpec.groovy
+++ grailsWorkshop/questionApp/src/test/groovy/com/opi/VoteControllerSpec.groovy
@@ -15,19 +15,4 @@ class VoteControllerSpec extends Specification {
     def cleanup() {
     }
 
-    void "voteUpQuestion should redirect to /question/show"() {
-        when:
-          controller.voteUpQuestion(42)
-
-        then:
-            response.redirectedUrl == '/question/show/42'
-    }
-
-    void "voteDownQuestion should redirect to /question/show"() {
-        when:
-          controller.voteDownQuestion(42)
-
-        then:
-            response.redirectedUrl == '/question/show/42'
-    }
 }
